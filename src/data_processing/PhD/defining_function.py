e7b4f225b4d73504296b1ae751eb96049b6f6cca
abdalraheem-ijjeh <aijjeh@imp.gda.pl>
2019-12-31 09:38:15 +0100

    buliding a basic CNN

--- src/data_processing/PhD/defining_function.py
+++ src/data_processing/PhD/defining_function.py
@@ -1,14 +1,10 @@
 import cv2
 from glob import glob
 import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-from PIL import Image
 import os
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K
 
 
 # In this part we import All images from all outputs_RMS_wave_dataset1_out
@@ -24,7 +20,7 @@ def func(path):
         dim = (width, height)
         img = cv2.resize(img, dim)
         # Crop the image to get the right upper corner to produce 256*256 image size
-        crop_img = img[0:256, 256:511]
+        crop_img = img[0:255, 256:511]
         #cv2.imshow("cropped", crop_img)
         #cv2.waitKey(0)
         #print(crop_img.size)
@@ -37,27 +33,32 @@ def func(path):
 
 
 # Reads the training images and store then in Img_train
-img_mask = 'E:/TestDataset/raw/num/Dataset_Project/train_data/*/RMS_flat_shell_Vz_*_*top.png'
-Img_train = func(img_mask)
+img_mask = 'E:/TestDataset/raw/num/train_data/*/RMS_flat_shell_Vz_*_500x500top.png'
+Img_train = func(img_mask)                  
 Training_IMG = np.asarray(Img_train)
+Training_IMG = Training_IMG/255 -0.5
 print('Training Images: ', Training_IMG.shape)
 
 # Reads the labels images for train images and store then in Train_label
-img_mask = 'E:/TestDataset/raw/num/Dataset_Project/train_labels/*.jpg'
-Train_label = func(img_mask)
-Label_Train = np.asarray(Train_label)
+###img_mask = 'E:/TestDataset/raw/num/Dataset_Project/train_labels/*.jpg'
+###Train_label = func(img_mask)
+###Label_Train = np.asarray(Train_label)
+
+Label_Train = np.arange(0,473,1)
 print('Label images :', Label_Train.shape)
 
 # Reads test data and store them in Img_test
-img_mask = 'E:/TestDataset/raw/num/Dataset_Project/test_data/*/*Vz_*_500x500top.png'
+img_mask = 'E:/TestDataset/raw/num/Dataset_Project/test_data/*/RMS_flat_shell_velocities_in_plane_*_500x500top.png'
 Img_test = func(img_mask)
 Test_IMG = np.asarray(Img_test)
+Test_IMG = Test_IMG /255 -0.5
 print("Test Image : ", Test_IMG.shape)
 
 # Reads the labels images for test images and store then in Test_label
-img_mask = 'E:/TestDataset/raw/num/Dataset_Project/test_labels/*.jpg'
-Test_label = func(img_mask)
-Label_Test = np.asarray(Test_label)
+##img_mask = 'E:/TestDataset/raw/num/Dataset_Project/test_labels/*.jpg'
+##Test_label = func(img_mask)
+##Label_Test = np.asarray(Test_label)
+Label_Test = np.arange(0,49,1)
 print("Label test", Label_Test.shape)
 
 #cv2.imshow('image', Img_train[100])
@@ -66,7 +67,37 @@ print("Label test", Label_Test.shape)
 #cv2.destroyAllWindows()
 #print('training Image :', Img_train[100].shape)
 
-# Normalizing input
-Training_IMG = Training_IMG/255 -.5
-Test_IMG = Test_IMG /255 -.5
+# Starting the Model
+#  We need to ‘one-hot-encode’ our target variable.
+#  This means that a column will be created for each
+#  output category and a binary variable is inputted for each category.
 
+from keras.utils import to_categorical
+Label_Train = to_categorical(Label_Train)
+
+print(Label_Train.shape)
+Label_Test = to_categorical(Label_Test)
+print(Label_Test.shape)
+
+
+# Create the model
+
+model = Sequential()
+
+#add model layers
+
+model.add(Conv2D(64, kernel_size=3, activation='relu',input_shape=(255,255,3)))
+model.add(Conv2D(32, kernel_size=3, activation='relu'))
+model.add(Flatten())
+model.add(Dense(473,activation='softmax'))
+
+# Compile model using accuracy to measure model performance
+model.compile(
+    optimizer='adam',
+    loss = 'categorical_crossentropy',
+    metrics=['accuracy'])
+# Training the model
+
+model.fit(Training_IMG,Label_Train, validation_data=(Training_IMG,Label_Train), epochs= 5)
+
+model.predict(Test_IMG[33])
