a3d6c6d113b70314716cd034f16d59271dbfb4ed
abdalraheem-ijjeh <aijjeh@imp.gda.pl>
2020-01-21 11:02:43 +0100

    Initial model to semantic segmentation implemented with Unet with skip connections_ updated with increasing validation set

--- src/data_processing/PhD/Unet_with_skip_connections.py
+++ src/data_processing/PhD/Unet_with_skip_connections.py
@@ -17,12 +17,12 @@ x = x.reshape (475,512,512,1)
 y = np.load ('E:/src/Ground Truth.npy')
 y = y.reshape (475,512,512,1)
 
-x_train = x[:348]
-val_x_train = x[348:379]
+x_train = x[:379]
+#val_x_train = x[348:379]
 test_x_samples = x[379:475]
 
-y_train = y[0:348]
-val_y_train = y[348:379]
+y_train = y[0:379]
+#val_y_train = y[348:379]
 tests_y_samples = y[379:475]
 
 inputs = Input(shape = (512, 512, 1))
@@ -30,14 +30,14 @@ inputs = Input(shape = (512, 512, 1))
 conv1 = Conv2D (filters = 16, kernel_size = (3,3), strides = 1, padding = 'same', activation = 'relu')(inputs)
 conv2 = Conv2D (filters = 16, kernel_size = (3,3), strides = 1, padding = 'same', activation = 'relu')(conv1)
 
-#added1 = keras.layers.Add()([conv1,conv3])
+added1 = keras.layers.Add()([conv1,conv2])
 
-p1 = (MaxPool2D ((2,2), strides = (2,2)))(conv2)
+p1 = (MaxPool2D ((2,2), strides = (2,2)))(added1)
 
 conv4 = (Conv2D (filters = 16, kernel_size = (3,3), strides = 1, padding = 'same', activation = 'relu'))(p1)
-conv5 = (Conv2D (filters = 16 , kernel_size = (3,3), strides = 1, padding = 'same', activation = 'relu'))(conv4)
+#conv5 = (Conv2D (filters = 16 , kernel_size = (3,3), strides = 1, padding = 'same', activation = 'relu'))(conv4)
 
-up1 = UpSampling2D ((2,2))(conv5)
+up1 = UpSampling2D ((2,2))(conv4)
 
 conv7 = (Conv2D (filters = 16, kernel_size = (3,3), strides = 1, padding = 'same', activation = 'relu'))(up1)
 conv8 = (Conv2D (filters = 16, kernel_size = (3,3), strides = 1, padding = 'same', activation ='relu'))(conv7)
@@ -48,6 +48,6 @@ output = (Conv2D (1, (1, 1), padding ='same', activation='sigmoid'))(conv8)
 model = Model(inputs = inputs, outputs = output)
 
 model.compile (optimizer ='adam', loss ='binary_crossentropy', metrics = ['acc'])
-model.fit (np.array (x_train), np.array (y_train), batch_size = 16, epochs = 5, validation_split=0.1)
+model.fit (np.array (x_train), np.array (y_train), batch_size = 16, epochs = 3, validation_split=0.2)
 model.summary ()
 model.save('Unet_skip.h5')
