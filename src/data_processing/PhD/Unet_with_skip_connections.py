90cc155d9bc936ec27328b80e05e1c08867211a0
abdalraheem-ijjeh <aijjeh@imp.gda.pl>
2020-01-21 10:51:13 +0100

    Initial model to semantic segmentation implemented with Unet with skip connections

---
+++ src/data_processing/PhD/Unet_with_skip_connections.py
@@ -0,0 +1,53 @@
+import numpy as np
+import cv2
+from glob import glob
+import gc
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPool2D, Flatten, UpSampling2D, Input, merge
+from keras.models import Model
+from keras.optimizers import adam, rmsprop, sgd
+
+# Force the Garbage Collector to release unreferenced memory
+gc.collect ()
+
+
+x = np.load ('E:/src/Training samples.npy')
+x = x.reshape (475,512,512,1)
+y = np.load ('E:/src/Ground Truth.npy')
+y = y.reshape (475,512,512,1)
+
+x_train = x[:348]
+val_x_train = x[348:379]
+test_x_samples = x[379:475]
+
+y_train = y[0:348]
+val_y_train = y[348:379]
+tests_y_samples = y[379:475]
+
+inputs = Input(shape = (512, 512, 1))
+
+conv1 = Conv2D (filters = 16, kernel_size = (3,3), strides = 1, padding = 'same', activation = 'relu')(inputs)
+conv2 = Conv2D (filters = 16, kernel_size = (3,3), strides = 1, padding = 'same', activation = 'relu')(conv1)
+
+#added1 = keras.layers.Add()([conv1,conv3])
+
+p1 = (MaxPool2D ((2,2), strides = (2,2)))(conv2)
+
+conv4 = (Conv2D (filters = 16, kernel_size = (3,3), strides = 1, padding = 'same', activation = 'relu'))(p1)
+conv5 = (Conv2D (filters = 16 , kernel_size = (3,3), strides = 1, padding = 'same', activation = 'relu'))(conv4)
+
+up1 = UpSampling2D ((2,2))(conv5)
+
+conv7 = (Conv2D (filters = 16, kernel_size = (3,3), strides = 1, padding = 'same', activation = 'relu'))(up1)
+conv8 = (Conv2D (filters = 16, kernel_size = (3,3), strides = 1, padding = 'same', activation ='relu'))(conv7)
+
+
+output = (Conv2D (1, (1, 1), padding ='same', activation='sigmoid'))(conv8)
+
+model = Model(inputs = inputs, outputs = output)
+
+model.compile (optimizer ='adam', loss ='binary_crossentropy', metrics = ['acc'])
+model.fit (np.array (x_train), np.array (y_train), batch_size = 16, epochs = 5, validation_split=0.1)
+model.summary ()
+model.save('Unet_skip.h5')
