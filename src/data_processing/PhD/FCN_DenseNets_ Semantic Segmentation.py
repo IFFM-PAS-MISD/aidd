86e0d5a167bd3c379f4293d17c87eb826af0b1c7
abdalraheem-ijjeh <aijjeh@imp.gda.pl>
2020-02-01 16:55:11 +0100

    FCN_DenseNets_fully_SkipConnections

--- src/data_processing/PhD/FCN_DenseNets_ Semantic Segmentation.py
+++ src/data_processing/PhD/FCN_DenseNets_ Semantic Segmentation.py
@@ -2,13 +2,13 @@ import numpy as np
 from keras.layers import Dense, Conv2D, MaxPool2D, Flatten, UpSampling2D, Input, merge
 from keras.models import Model
 import keras
-import tensorflow as tf
 from sklearn.utils import shuffle
+import tensorflow as tf
 import tensorflow.keras.backend as K
-
 import gc
 
-gc.collect()
+
+
 
 # layers
 def layer(Layer_input):
@@ -16,6 +16,7 @@ def layer(Layer_input):
     CN = keras.layers.Conv2D(filters=16, kernel_size=(3, 3), padding='same', activation='relu')(BN)  # Convolution
     out = keras.layers.Dropout(0.2)(CN)  # Dropout
     return out
+
 
 # Dense Block
 def dense_block(DB_input, layers):
@@ -27,7 +28,8 @@ def dense_block(DB_input, layers):
     out = Concat
     return out
 
-# Transition Down (Maxpooling)
+
+# Transition Down (Max-pooling)
 def Transition_Down(TD_input):
     BN = keras.layers.BatchNormalization()(TD_input)
     CN = keras.layers.Conv2D(filters=16, kernel_size=(1, 1), activation='relu')(BN)
@@ -35,35 +37,44 @@ def Transition_Down(TD_input):
     down = keras.layers.MaxPooling2D((2, 2), strides=(2, 2))(Drop)
     return down
 
+
 # Transition Up (Unsampling)
 def Transition_Up(TU_input):
-    #Up = keras.layers.Conv2DTranspose(16, (3, 3), strides=(2, 2))(TU_input)
-    Up = keras.layers.UpSampling2D(size=(2,2))(TU_input)
+    # Up = keras.layers.Conv2DTranspose(16, (3, 3), strides=(2, 2))(TU_input)
+    Up = keras.layers.UpSampling2D(size=(2, 2))(TU_input)
     return Up
 
+
 # Keras Model (FCN Dense Net for semantic Segmentation
-def DenseNet_Model(xtrain, ytrain, DB_Num):
+def DenseNet_Model(x_train, y_train, DB_Num):
     inputs = Input(shape=(512, 512, 1))
     DB1 = dense_block(inputs, DB_Num[0])
-    TD1 = Transition_Down(DB1)
+    Concat1 = keras.layers.Concatenate()([inputs,DB1])
+    TD1 = Transition_Down(Concat1)
     DB2 = dense_block(TD1, DB_Num[1])
-    TD2 = Transition_Down(DB2)
+    Concat2 = keras.layers.Concatenate()([TD1,DB2])
+    TD2 = Transition_Down(Concat2)
     DB3 = dense_block(TD2, DB_Num[2])
     TU1 = Transition_Up(DB3)
+    #Concat3 = keras.layers.Concatenate()([TU1, Concat2])
     DB4 = dense_block(TU1, DB_Num[3])
     TU2 = Transition_Up(DB4)
+    #Concat4 = keras.layers.Concatenate()([TU2,Concat1])
     DB5 = dense_block(TU2, DB_Num[4])
     output = keras.layers.Conv2D(1, (1, 1), padding='same', activation='sigmoid')(DB5)
-    model = Model(inputs=inputs, outputs=output)
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
-    model.fit(xtrain, ytrain, batch_size=4, epochs=5, validation_split=0.1)
-    model.summary()
-    return model
+
+    segment_model = Model(inputs=inputs, outputs=output)
+    segment_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+    segment_model.fit(x_train, y_train, batch_size=5, epochs=5, validation_split=0.1)
+    segment_model.summary()
+
+    return segment_model
+
 
 #####################################
 # Loading the dataset
 #####################################
-x = np.load('E:/src/datasets/Segmentation datasets/augemented/Augmented_data_segmentation.npy')
+x = np.load('E:/src/datasets/Segmentation datasets/augemented/Augmented_data_segmentation_New_updates.npy')
 x = x.reshape(1900, 512, 512, 1)
 y = np.load('E:/src/datasets/Segmentation datasets/augemented/Augmented_target_segmentation.npy')
 y = y.reshape(1900, 512, 512, 1)
@@ -71,7 +82,7 @@ y = y.reshape(1900, 512, 512, 1)
 #####################################
 # Randomly shuffle the dataset
 #####################################
-x, y = shuffle(x,y)
+x, y = shuffle(x, y)
 #####################################
 x_train = x[:1520]
 y_train = y[:1520]
@@ -81,8 +92,7 @@ tests_y_samples = y[1520:]
 #####################################
 DB_Number = [3, 3, 3, 3, 3]
 print(len(DB_Number))
-x_train = x_train#K.variable(x_train)
-y_train = y_train#K.variable(y_train)
-L = 10
 model = DenseNet_Model(x_train, y_train, DB_Number)
 model.save('FCN_DsensNets_Semantic_Segmentation.h5')
+
+gc.collect()
