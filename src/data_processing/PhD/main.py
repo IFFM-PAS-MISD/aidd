b486fe5f4f915ff1fd21413e5e614c02107f71a5
abdalraheem-ijjeh <aijjeh@imp.gda.pl>
2020-02-03 15:36:00 +0100

    Update main.py
    adding plotting fuctions fot training and exprimental

--- src/data_processing/PhD/main.py
+++ src/data_processing/PhD/main.py
@@ -16,13 +16,14 @@ gc.collect()
 #####################################
 # Loading dataset into x, y arrays and reshape them
 #####################################
-x = np.load('E:/src/datasets/Segmentation datasets/augemented/Augmented_data_segmentation_New_updates.npy')
+x = np.load('E:/backup/datasets/Segmentation datasets/augemented/Augmneted_train_new_data_RMS_flat_shell_bottom.npy')
 x = x.reshape(1900, 512, 512, 1)
-y = np.load('E:/src/datasets/Segmentation datasets/augemented/Augmented_target_segmentation.npy')
+y = np.load('E:/backup/datasets/Segmentation datasets/augemented/Augmented_target_segmentation.npy')
 y = y.reshape(1900, 512, 512, 1)
 #####################################
-experimental = np.load('E:/src/datasets/expermintal/test_images.npy')
-experimental = experimental.reshape(46, 512, 512, 1)
+experimental = np.load('Experimental_test_images.npy')
+print(experimental.shape)
+experimental = experimental.reshape(276, 512, 512, 1)
 #####################################
 # Shuffle the data set at random
 #####################################
@@ -40,7 +41,7 @@ test_x_samples, tests_y_samples = shuffle(test_x_samples, tests_y_samples)
 #####################################
 # Loading the model
 #####################################
-model = load_model('FCN_DsensNets_Semantic_Segmentation.h5')
+model = load_model('E:/backup/models/FCN_DsensNets_Semantic_Segmentation_filter16_epoch5_kernal(3, 3)_drpout0.2_batch_size_4.h5')
 model.summary()
 #####################################
 # Evaluating the model using test set
@@ -54,31 +55,44 @@ print('Test accuracy:', score[1])
 m_IoU = 0
 count = 0
 #####################################
-prediction = model.predict(test_x_samples, batch_size=1)
-prediction = np.asarray(prediction)
-#####################################
-for i in range(380):
-    damage = np.squeeze(prediction[i], axis=2)
-    original = np.squeeze(test_x_samples[i], axis=2)
-    # original = np.squeeze(experimental[i], axis= 2)
-    mask = np.squeeze(tests_y_samples[i], axis=2)
-    fig = plt.figure(figsize=(16, 9))
-    ax1 = fig.add_subplot(1, 3, 1)
-    plt.imshow(damage, cmap='tab20c')
-    ax2 = fig.add_subplot(1, 3, 2)
-    plt.imshow(original, cmap='gist_yarg')
-    ax3 = fig.add_subplot(1, 3, 3)
-    plt.imshow(mask, cmap='gist_gray')
-    ax1.title.set_text('Detected Damage')
-    ax2.title.set_text('Original input Image')
-    ax3.title.set_text('Ground Truth / Label')
-    plt.show()
+def Training():
+    prediction = model.predict(tests_y_samples, batch_size=1)
+    prediction = np.asarray(prediction)
     #####################################
-    # cv2.imshow('Detected  damage', prediction[i])
-    # cv2.imshow('GT', tests_y_samples[i])
-    # cv2.imshow('original image', test_x_samples[i])
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+    for i in range(380):
+        damage = np.squeeze(prediction[i], axis=2)
+        original = np.squeeze(test_x_samples[i], axis=2)
+        mask = np.squeeze(tests_y_samples[i], axis=2)
+        fig = plt.figure(figsize=(16, 9))
+        ax1 = fig.add_subplot(1, 3, 1)
+        plt.imshow(damage, cmap='tab20c')
+        ax2 = fig.add_subplot(1, 3, 2)
+        plt.imshow(original, cmap='gist_yarg')
+        ax3 = fig.add_subplot(1, 3, 3)
+        plt.imshow(mask, cmap='gist_gray')
+        ax1.title.set_text('Detected Damage')
+        ax2.title.set_text('Original input Image')
+        ax3.title.set_text('Ground Truth / Label')
+        plt.show()
+        #####################################
+
+def exp():
+    prediction = model.predict(experimental, batch_size=1)
+    prediction = np.asarray(prediction)
     #####################################
-# garbage collector
+    for i in range(380):
+        damage = np.squeeze(prediction[i], axis=2)
+        original = np.squeeze(experimental[i], axis=2)
+        fig = plt.figure(figsize=(16, 9))
+        ax1 = fig.add_subplot(1, 2, 1)
+        plt.imshow(damage, cmap='cool')
+        ax2 = fig.add_subplot(1, 2, 2)
+        plt.imshow(original, cmap='gist_yarg')
+        ax1.title.set_text('Detected Damage')
+        ax2.title.set_text('Original input Image')
+        plt.show()
+        #####################################
+
+#Training()
+exp()
 gc.collect()
