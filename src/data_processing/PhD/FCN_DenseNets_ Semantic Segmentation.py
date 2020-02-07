4e12a2cf48eb8b3a34f090beeca6eb01d06d6325
abdalraheem-ijjeh <aijjeh@imp.gda.pl>
2020-02-07 15:20:27 +0100

    Adding two extra layers to the model to become- 7 DBs  FCN_DenseNet

--- src/data_processing/PhD/FCN_DenseNets_ Semantic Segmentation.py
+++ src/data_processing/PhD/FCN_DenseNets_ Semantic Segmentation.py
@@ -1,36 +1,33 @@
 import numpy as np
-from keras.layers import Dense, Conv2D, MaxPool2D, Flatten, UpSampling2D, Input, merge
+from keras.layers import Dense, Conv2D, MaxPool2D, Flatten, UpSampling2D, Input, merge, Activation, BatchNormalization
 from keras.models import Model
 from keras.activations import relu, sigmoid, softmax
-from keras.optimizers import adam, RMSprop
-from keras.losses import categorical_crossentropy, binary_crossentropy, mean_squared_error
 import keras
 import gc
 from sklearn.utils import shuffle
-from keras.backend.tensorflow_backend import set_session
 import tensorflow as tf
 from keras import backend as K
 
-
 # Hyper parameters
 #####################################
+
 lr = .0001
 rho = 0.995
 filters = 16
 filterSize = (3, 3)
 activation = relu, sigmoid
-batch_size = 2
+batch_size = 4
 dropout = 0.2
-epochs = 5
+epochs = 10
 validation_split = 0.1
 #####################################
 # Loading the dataset
 #####################################
-x = np.load('E:/backup/datasets/Segmentation datasets/augemented/Augmneted_train_new_data_RMS_flat_shell_top.npy')
-x=x/255.
+x = np.load('E:/backup/datasets/Segmentation datasets/augemented/Augmneted_train_new_data_RMS_flat_shell_bottom.npy')
+x = x / 255.
 x = x.reshape(1900, 512, 512, 1)
 y = np.load('E:/backup/datasets/Segmentation datasets/augemented/Augmented_target_segmentation.npy')
-y=y/255.
+y = y / 255.
 y = y.reshape(1900, 512, 512, 1)
 
 #####################################
@@ -40,12 +37,15 @@ x, y = shuffle(x, y)
 #####################################
 x_train = x[:1520]
 y_train = y[:1520]
+
+
 ######################################
 # layers
 def layer(Layer_input):
     BN = keras.layers.BatchNormalization()(Layer_input)  # Batch Normalization
-    CN = keras.layers.Conv2D(filters=filters, kernel_size=filterSize, padding='same', activation='relu')(
-        BN)  # Convolution
+    x = Activation('relu')(BN)  # adding activation layer Relu then directs it to the Conv2D
+    CN = keras.layers.Conv2D(filters=filters, kernel_size=filterSize, kernel_initializer='he_normal', padding='same')(
+        x)  # adding kernal_intializer ,activation='relu'
     out = keras.layers.Dropout(dropout)(CN)  # Dropout
     return out
 
@@ -53,11 +53,13 @@ def layer(Layer_input):
 # Dense Block
 def dense_block(DB_input, layers):
     global Concat
-    activate = Dense(1,activation='relu')(DB_input)
+    #x = BatchNormalization()(DB_input)
+    #x = Activation('relu')(x)
+    # activate = Dense(1,activation='relu')(DB_input)
     for i in range(layers):
-        temp = layer(activate)
-        Concat = keras.layers.Concatenate(axis=-1)([temp, activate])
-        activate = temp
+        temp = layer(DB_input)
+        Concat = keras.layers.Concatenate(axis=-1)([temp, DB_input])
+        DB_input = temp
     out = Concat
     return out
 
@@ -65,34 +67,36 @@ def dense_block(DB_input, layers):
 # Transition Down (Max-pooling)
 def Transition_Down(TD_input):
     BN = keras.layers.BatchNormalization()(TD_input)
-    active = Dense(1, activation="relu")(BN)
-    CN = keras.layers.Conv2D(filters=filters, kernel_size=(1,1), padding='same')(active)
+    active = Activation('relu')(BN)
+    CN = keras.layers.Conv2D(filters=filters, kernel_size=(1, 1), padding='same')(active)  # , activation='relu'
     Drop = keras.layers.Dropout(dropout)(CN)
     down = keras.layers.MaxPooling2D((2, 2), strides=(2, 2))(Drop)
     return down
 
 
-# Transition Up (Unsampling)
+# Transition Up (Up sampling)
 def Transition_Up(TU_input):
-    #Up = keras.layers.Conv2DTranspose(filters, (2, 2), strides=(2, 2))(TU_input)
-    Up = keras.layers.Convolution2DTranspose(filters=filters, kernel_size=(3, 3), strides=(2, 2), padding='same')(TU_input)
+    Up = keras.layers.Convolution2DTranspose(filters=filters, kernel_size=(3, 3), strides=(2, 2), padding='same')( ### maybe we need to make it vaild
+        TU_input)
     return Up
 
 
 ###################
 # Custom loss function
-def custom_loss(y_true, y_pred,smooth =1):  #Dice score function
+def custom_loss(y_true, y_pred, smooth=1):  # Dice score function
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
     return -(2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+
 ###################
 # Custom metric
 def iou_metric(y_true, y_pred, smooth=1):
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(K.abs(y_true_f * y_pred_f))
-    union = K.sum(y_true_f) + K.sum(y_pred_f) - intersection
+    union = K.sum(K.abs(y_true_f)) + K.sum(K.abs(y_pred_f)) - intersection
     iou = (intersection + smooth) / (union + smooth)
     return iou
 
@@ -100,30 +104,50 @@ def iou_metric(y_true, y_pred, smooth=1):
 # Keras Model (FCN Dense Net for semantic Segmentation
 def DenseNet_Model(x_train, y_train, DB_Num):
     inputs = Input(shape=(512, 512, 1))
-    DB1 = dense_block(inputs, DB_Num[0])
-    Concat1 = keras.layers.Concatenate(axis=-1)([inputs, DB1])
+
+    Conv = Conv2D(filters, filterSize, padding='same', activation='relu')(inputs)
+    Conv = Conv2D(filters, filterSize, padding='same', activation='relu')(inputs)
+    Conv = Conv2D(filters, filterSize, padding='same', activation='relu')(inputs)
+    Conv = keras.layers.Concatenate()([Conv,inputs])
+
+    DB1 = dense_block(Conv, DB_Num[0])
+    Concat1 = keras.layers.Concatenate(axis=-1)([Conv, DB1])
     TD1 = Transition_Down(Concat1)
     DB2 = dense_block(TD1, DB_Num[1])
     Concat2 = keras.layers.Concatenate(axis=-1)([TD1, DB2])
-    TD2 = Transition_Down(DB2)
+    TD2 = Transition_Down(Concat2)  # here was DB2
     DB3 = dense_block(TD2, DB_Num[2])
-    TU1 = Transition_Up(DB3)
-    Concat3 = keras.layers.Concatenate(axis=-1)([TU1, Concat2])
-    DB4 = dense_block(Concat3, DB_Number[3])
-    TU2 = Transition_Up(DB4)
-    Concat4 = keras.layers.Concatenate(axis=-1)([TU2, Concat1])
+    ############## new addition
+    Concat3  = keras.layers.Concatenate(axis=-1)([TD2, DB3])
+    TD3 = Transition_Down(Concat3)
+    DB4 = dense_block(TD3, DB_Num[3])
+
+    ##############
+    TU1 = Transition_Up(DB4)
+    Concat4 = keras.layers.Concatenate(axis=-1)([TU1, Concat3])
     DB5 = dense_block(Concat4, DB_Number[4])
-    output = keras.layers.Conv2D(1, (1, 1), padding='same', activation='sigmoid')(DB5)
+    TU2 = Transition_Up(DB5)
+    Concat5 = keras.layers.Concatenate(axis=-1)([TU2, Concat2])
+    DB6 = dense_block(Concat5, DB_Number[5])
+    TU3 = Transition_Up(DB6)
+    Concat6 = keras.layers.Concatenate(axis=-1)([TU3, Concat1])
+    DB7 = dense_block(Concat6,DB_Number[6])
+
+    output = keras.layers.Conv2D(1, (1, 1), activation='sigmoid', padding='same')(DB7)  #activation='sigmoid',
 
     segment_model = Model(inputs=inputs, outputs=output)
     segment_model.compile(optimizer='adam', loss=custom_loss, metrics=[iou_metric])
     segment_model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=validation_split)
     segment_model.summary()
     return segment_model
-DB_Number = [4, 4, 4, 4, 4]
+
+
+DB_Number = [4, 4, 4, 4, 4, 4, 4]  # adding extra two DBs [0,1,2,3,4,5,6]
 print(len(DB_Number))
 model = DenseNet_Model(x_train, y_train, DB_Number)
-model.save('E:/backup/models/FCN_DenseNet_models/FCN_DsensNets_Semantic_Segmentation_filter_Using_Conv2DTranspose' + str(filters) + '_epoch_' + str(epochs) + '_kernal_' + str(
-    filterSize) + '_drpout_' + str(dropout) +'_batch_size_'+str(batch_size) +'_loss_updated.h5')
+model.save(
+    'E:/backup/models/FCN_DenseNet_models/FCN_DsensNets_Semantic_Segmentation_filter_Using_Conv2DTranspose' + str(
+        filters) + '_epoch_' + str(epochs) + '_kernal_' + str(
+        filterSize) + '_drpout_' + str(dropout) + '_batch_size_' + str(batch_size) + '_loss_updated_changed_DB _layer.h5')
 
 gc.collect()
